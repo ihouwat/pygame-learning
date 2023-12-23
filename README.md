625418282b2cae83612b5a635b9d175362d94bc0
Igor <houwat@gmail.com>
2023-12-23 12:52:16 -0500

    restructure project to isolate tutorials

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # Pygame
 
 Folder structure:
-* `000_game-loop`: the Game Loop and how to initialize and end a pygame program
-* `001_drawing`: surfaces, displaying images
-* `002_events`: simple event handling
-* `003_car-game`: a small game where the player controls a car and avoids crashing into oncoming traffic
-* `004_rpg`: a simple RPG game with a player, enemies, and a dungeon
-* `005_same_again`: a matching and language skills game for my kiddo, my own code
+* `same_again`: a matching and language skills game for my kiddo, my own code
+* `tutorials folder`:
+	* `000_game-loop`: the Game Loop and how to initialize and end a pygame program
+	* `001_drawing`: surfaces, displaying images
+	* `002_events`: simple event handling
+	* `003_car-game`: a small game where the player controls a car and avoids crashing into oncoming traffic
+	* `004_rpg`: a simple RPG game with a player, enemies, and a dungeon
---
+++ tutorials/001_drawing/drawing-tutorial.py
@@ -0,0 +1,49 @@
+import os
+import pathlib
+
+import pygame
+from pygame.locals import QUIT
+
+
+class App:
+	def __init__(self) -> None:
+		self._running = True
+		self._display_surf = None
+		self._image_surf = None
+	
+	def on_init(self):
+		pygame.init()
+		self._display_surf = pygame.display.set_mode((350, 350), pygame.HWSURFACE)
+		self._running = True
+		# load the image and put it on a new Surface object.
+		self._image_surf = pygame.image.load(os.path.join(pathlib.Path(__file__).parent.absolute(), 'image.jpg')).convert()
+	
+	def on_event(self, event):
+		if event.type == QUIT:
+			self._running = False
+	
+	def on_loop(self):
+		pass
+
+	def on_render(self):
+		# put the uploaded image on the display surface, starting at the top left corner.
+		self._display_surf.blit(self._image_surf, (0, 0))
+		pygame.display.flip()
+	
+	def on_cleanup(self):
+		pygame.quit()
+	
+	def on_execute(self):
+		if self.on_init() is False:
+			self._running = False
+
+		while( self._running ):
+			for event in pygame.event.get():
+				self.on_event(event)
+			self.on_loop()
+			self.on_render()
+		self.on_cleanup()
+  
+if __name__ == "__main__":
+	theApp = App()
+	theApp.on_execute()
