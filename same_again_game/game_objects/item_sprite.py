4b36f7250d5e4e343357adf6abc541ad404b2dc2
Igor <houwat@gmail.com>
2023-12-24 13:53:05 -0500

    add factory method in ItemSprite to create clones

--- same_again_game/game_objects/item_sprite.py
+++ same_again_game/game_objects/item_sprite.py
@@ -22,6 +22,12 @@ class ItemSprite(pygame.sprite.Sprite):
     super().__init__()
     self.rect = self.image.get_rect()
   
+  @classmethod
+  # 'ItemSprite' is a forward reference, a string that contains the class name of a class that hasn't been fully defined at the point we're adding the type hint
+  def create_from(cls, sprite: 'ItemSprite') -> 'ItemSprite':
+    """ Creates a copy of an item sprite."""
+    return cls(image=sprite.image, text_identifier=sprite.text_identifier, word=sprite.word)
+  
   # By default, dataclass() will not implicitly add a __hash__() method unless it is safe to do so.
   # we add this method in order to use instances of the class in pygame sprite groups
   def __hash__(self) -> int:
