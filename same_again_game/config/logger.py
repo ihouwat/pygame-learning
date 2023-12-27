e4bbdc1b81fddf373f5e31c5590fc8ff1c3fd71a
Igor <houwat@gmail.com>
2023-12-27 16:56:20 -0500

    further refine logger config

--- same_again_game/config/logger.py
+++ same_again_game/config/logger.py
@@ -1,5 +1,5 @@
 import logging
 
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(module)s module, line #%(lineno)d - %(message)s')
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(module)s, %(lineno)d: %(message)s')
 logging.getLogger("app_logger")
 logger = logging.getLogger("app_logger")
