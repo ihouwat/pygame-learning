50e148ba77e69029bc4011707764a50717399118
Igor <houwat@gmail.com>
2023-12-29 16:02:03 -0500

    create text element class

---
+++ same_again_game/game_objects/text_element.py
@@ -0,0 +1,9 @@
+class TextElement:
+	def __init__(self, text, font, color, x, y, anchor_x="center", anchor_y="center"):
+		self.text = text
+		self.font = font
+		self.color = color
+		self.x = x
+		self.y = y
+		self.anchor_x = anchor_x # 
+		self.anchor_y = anchor_y
