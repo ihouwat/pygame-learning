d59275bb11211692b8f56484323c1a4255feb208
Igor <houwat@gmail.com>
2023-12-30 14:18:50 -0500

    enhance the TextElement class to expose the pygame Surface

--- same_again_game/game_objects/text_element.py
+++ same_again_game/game_objects/text_element.py
@@ -3,14 +3,16 @@ import pygame
 
 class TextElement:
 	def __init__(self, text: str, font: pygame.font.Font, color: pygame.Color, x: int, y: int):
-		self.text = text
-		self.font = font
-		self.color = color
-		self.start_position = (x, y)
-		self.current_position = (x, y)
+		self.text: str = text
+		self.font: pygame.font.Font = font
+		self.color: pygame.Color = color
+		self.start_position: tuple[int, int] = (x, y)
+		self.current_position: tuple[int, int] = (x, y)
+		self.surface: pygame.Surface = self.font.render(self.text, True, self.color)
 	
 	def set_text(self, text: str) -> None:
 		self.text = text
+		self.surface = self.font.render(self.text, True, self.color)
 	
 	def set_position(self, x: int, y: int) -> None:
 		self.current_position = (x, y)
@@ -19,4 +21,4 @@ class TextElement:
 		self.current_position = self.start_position
 	
 	def draw(self) -> pygame.Surface:
-		return self.font.render(self.text, True, self.color)
+		return self.surface
