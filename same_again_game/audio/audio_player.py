46ddb8909c87dac8a5d31f14e9a27002713a63bd
Igor <houwat@gmail.com>
2024-01-01 17:13:17 -0500

    tidy up order of methods in `AudioPlayer`

--- same_again_game/audio/audio_player.py
+++ same_again_game/audio/audio_player.py
@@ -7,7 +7,7 @@ from pygame import mixer
 class AudioPlayer:
 	def __init__(self) -> None:
 		mixer.init(frequency=44100)
-		self.soundtrack: list[SoundEffect] = []
+		self.sound_effects: list[SoundEffect] = []
 		
 	def playsoundtrack(self, filepath: str, iterations: int, volume: float = 0.5):
 		track = self.load_music(filepath)
@@ -15,6 +15,9 @@ class AudioPlayer:
 		mixer.music.load(track)
 		mixer.music.play(iterations)
 
+	def stop_music(self) -> None:
+		mixer.music.stop()
+	
 	def playsound(self, path: str, volume: float) -> None:
 		sound, cached_sound = self.load_and_cache_sound(path)
 
@@ -25,12 +28,6 @@ class AudioPlayer:
 		sound.play()
 		cached_sound.is_playing = True
 
-	def stop(self) -> None:
-		mixer.music.stop()
-	
-	def sound_is_cached(self, path: str) -> bool:
-		return bool([s for s in self.soundtrack if s.path == path])
-
 	def load_and_cache_sound(self, path: str) -> tuple[pygame.mixer.Sound, SoundEffect]:
 		sound: pygame.mixer.Sound
 		cached_sound: SoundEffect | None = self.get_cached_sound(path)
@@ -40,9 +37,12 @@ class AudioPlayer:
 		else:
 			sound = self.load_sound(path)
 			cached_sound = SoundEffect(sound=sound, path=path, is_playing=False)
-			self.soundtrack.append(cached_sound)
+			self.sound_effects.append(cached_sound)
 
 		return sound, cached_sound
+
+	def get_cached_sound(self, path: str) -> SoundEffect | None:
+		return next((s for s in self.sound_effects if s.path == path), None) # next() returns the next item in an iterator
 
 	def stop_sound(self, path: str) -> None:
 		cached_sound = self.get_cached_sound(path)
@@ -50,9 +50,6 @@ class AudioPlayer:
 			cached_sound.sound.stop()
 			cached_sound.is_playing = False
 	
-	def get_cached_sound(self, path: str) -> SoundEffect | None:
-		return next((s for s in self.soundtrack if s.path == path), None) # next() returns the next item in an iterator
-
 	def load_music(self, path) -> str:
 		return get_file('assets', 'music', path)
 
