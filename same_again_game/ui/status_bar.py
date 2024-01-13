8ea11eea7d9e5f64f31f0477a53a8f963c2b9d65
Igor <houwat@gmail.com>
2024-01-13 14:06:22 -0500

    Refactor status bar UI to use TextElements and clean up code

--- same_again_game/ui/status_bar.py
+++ same_again_game/ui/status_bar.py
@@ -3,6 +3,7 @@ from typing import Tuple
 
 import pygame
 from config.settings import FONT_NAME, FONT_SMALL
+from game_objects.text_element import TextElement
 from models.game_types import Color
 from ui.ui_display import UIDisplay
 
@@ -13,23 +14,40 @@ class StatusBar:
 	Attributes:
 		x_coordinate(int): The x coordinate of the status bar.
 		y_coordinate(int): The y coordinate of the status bar.
-		font(pygame.font.Font): The font to be used for the status bar.
-		text_color(pygame.Color): The color of the text.
+		font(pygame.font.Font): The font to be used for the text elements.
+		text_color(pygame.Color): The color of the text elements.
 		background_color(pygame.Color): The background color of the status bar.
+		player_name(TextElement): The text element for the player name.
+		language(TextElement): The text element for the language.
+		level(TextElement): The text element for the level.
+		score(TextElement): The text element for the score.
 	"""
 	def __init__(self, x_coordinate: int = 0, y_coordinate: int = 0):
 		self.x_coordinate = x_coordinate
-		self.y_coordinate = y_coordinate	
+		self.y_coordinate = y_coordinate
 		self.font = pygame.font.Font(pygame.font.match_font(FONT_NAME), FONT_SMALL)
 		self.text_color = pygame.Color(Color.WHITE.value)
 		self.background_color = pygame.Color('black')
+
+		# Initialize the text elements
+		self.player_name = TextElement('', self.font, self.text_color, 0, 0)
+		self.language = TextElement('', self.font, self.text_color, 0, 0)
+		self.level = TextElement('', self.font, self.text_color, 0, 0)
+		self.score = TextElement('', self.font, self.text_color, 0, 0)
 	
 	def update(self, ui_display: UIDisplay) -> list[Tuple[pygame.Surface, Tuple[int, int]]]:
-		""" Updates the score and level and returns the content of the status bar."""
-		player_name_surface = self.font.render(f'Good luck {ui_display.player_name}!', True, self.text_color, self.background_color)
-		language_surface = self.font.render(f'Language: {ui_display.language.value}', True, self.text_color, self.background_color)
-		level_surface = self.font.render(f'Level: {ui_display.level}', True, self.text_color, self.background_color)
-		score_surface = self.font.render(f'Score: {ui_display.score}', True, self.text_color, self.background_color)
+		""" Updates the status bar text content and returns the surfaces to be render.
+
+		Args:
+			ui_display(UIDisplay): The UI display object.
+			
+		Returns:
+			list[Tuple[pygame.Surface, Tuple[int, int]]]: The surfaces to be rendered and their coordinates
+		"""
+		self.player_name.set_text(f'Good luck {ui_display.player_name}!')
+		self.language.set_text(f'Language: {ui_display.language.value}')
+		self.level.set_text(f'Level: {ui_display.level}')
+		self.score.set_text(f'Score: {ui_display.score}')
 		
 		line_height = self.font.get_linesize()
-		return [(surface, (self.x_coordinate, self.y_coordinate + (line_height * i+1))) for i, surface in enumerate([player_name_surface, language_surface, level_surface, score_surface]) ]
+		return [(surface.draw(), (self.x_coordinate, self.y_coordinate + (line_height * i+1))) for i, surface in enumerate([self.player_name, self.language, self.level, self.score]) ]
